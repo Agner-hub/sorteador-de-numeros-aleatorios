99d6240a151bc37bf4768749f3bb3ef55dcda5e0
Agner-hub <agnerchristian884@gmail.com>
2025-07-04 19:35:47 -0300

    Math

--- settings/script.js
+++ settings/script.js
@@ -1,6 +1,6 @@
 function sorteio() {
   const min = Math.ceil(document.querySelector(".min").value);
-  const max = Math.floor(document.querySelector(".max").value);
+  const max = Math.floor(document.querySelector(".max").value);//esse Math é obrigatório
 
   let number = Math.floor(Math.random() * (max - min + 1)) + min;
   alert(number);
